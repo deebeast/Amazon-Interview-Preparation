091bda3fb9e514a80635add264d19c8416f47abf
Deepak Vishwakarma <deepakvnit16@users.noreply.github.com>
2017-06-15 14:33:58 +0530

    Create IsSquare.java

---
+++ Easy Level/IsSquare.java
@@ -0,0 +1,9 @@
+// A java program to check if four given points form a square or not.
+import java.util.Scanner;
+
+class IsSquare {
+	public static void main(String[] args {
+		Scanner scan = new Scanner(System.in);
+		
+	}
+}
